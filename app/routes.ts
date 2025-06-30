b0d6e9c43cd529c96a23db8950e7e90029682bf6
Nini-Natroshvili <natroshvili.nini@kiu.edu.ge>
2025-07-01 00:00:07 +0400

    fix react router context error caused by using context outside the context provider

--- app/routes.ts
+++ app/routes.ts
@@ -6,4 +6,5 @@ export default [
   route('product/:id', 'routes/product.$id.tsx'),
   route('cart', 'routes/cart.tsx'),
   route('checkout', 'routes/checkout.tsx'),
+  route('*', 'routes/$.tsx'),
 ] satisfies RouteConfig;
